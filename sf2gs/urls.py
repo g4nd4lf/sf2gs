7fa29499173e9255770418d6130d71fe2c663256
g4nd4lf <112165219+g4nd4lf@users.noreply.github.com>
2023-08-21 14:08:06 +0200

    updated gs url. Now /sf2gs/gs

--- sf2gs/urls.py
+++ sf2gs/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sf2gs/', include('sf2gs_app.urls') ),
-    path('gs/', include('gs_app.urls') )   
+    path('sf2gs/gs', include('gs_app.urls') )   
 ]
